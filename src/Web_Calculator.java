2e01a24603bb2cba97c3b29b0880bfd4739a1df8
gutsteip <gutsteip@tcd.ie>
2022-11-07 11:57:36 +0000

    Finished calculator functionality
    Added final pieces of calculator functionality and corrected a few errors in logic

--- src/Web_Calculator.java
+++ src/Web_Calculator.java
@@ -34,8 +34,10 @@ public class Web_Calculator {
 
     public static float evaluateExpression(String exp)
     {
-        Stack<Float> numStack = new Stack<Integer>();
+        Stack<Float> numStack = new Stack<Float>();
         Stack<Character> opStack = new Stack<Character>();
+        boolean isLnNumber = false;
+        boolean isExpNumber = false;
 		/*
 		Part 1:
 		-Evaluate expressions in parentheses first
@@ -53,19 +55,47 @@ public class Web_Calculator {
             //Get final number and push to the number stack
             if(i == expression.length()-1)
             {
-                currNum += c;
-                numStack.push(Integer.valueOf(currNum));
+                //Don't add parentheses to stack
+                if(c != ')')
+                    currNum += c;
+
+                numStack.push(Float.valueOf(currNum));
             }
             //If it is digit, add to current number
-            else if(isNumber(c))
+            else if(isNumber(c) || c == '.')
                 currNum += c;
-
+                //Logrithmic function
+            else if(c == 'l')
+            {
+                isLnNumber = true;
+                i++;
+            }
+            //Exponential function
+            else if(c == 'e')
+            {
+                isExpNumber = true;
+                i+=2;
+            }
             //If it's operator
             else if(isOperator(c))
             {
+                //Convert string to float
+                float valOfCurrNum = Float.valueOf(currNum);
+
+                //Check to see if valOfCurrNum needs to be ln or exp
+                if(isExpNumber)
+                    valOfCurrNum = (float) Math.exp(valOfCurrNum);
+                if(isLnNumber)
+                    valOfCurrNum = (float) Math.log(valOfCurrNum);
+
+                //Reset those variables right after
+                isExpNumber = false;
+                isLnNumber = false;
+
                 //Push current number (which has ended) to stack & reset currNum
-                numStack.push(Float.valueOf(currNum));
+                numStack.push(valOfCurrNum);
                 currNum = "";
+
                 //If operator stack isn't empty
                 if (!opStack.isEmpty())
                 {
@@ -88,6 +118,11 @@ public class Web_Calculator {
                     opStack.push(c);
             }
         }
+        //If last number was exp/ln
+        if(isExpNumber)
+            numStack.push((float) Math.exp(numStack.pop()));
+        if(isLnNumber)
+            numStack.push((float) Math.log(numStack.pop()));
 		/*
 		 Part 2:
 		 Do repeated operations until opStack is empty
@@ -128,8 +163,8 @@ public class Web_Calculator {
                     String insideWithParen = expression.substring(i, indexCloseParen+1);
                     String insideParen = expression.substring(i+1, indexCloseParen);
 
-                    //Evaluate expression inside of parentheses (will call recusively)
-                    String evaledParentheses = evaluateExpression(insideParen).toString;
+                    //Evaluate expression inside of parentheses (will call recursively)
+                    String evaledParentheses = Float.toString(evaluateExpression(insideParen));
 
                     //Replace the string with the new String
                     newExp = newExp.replace(insideWithParen, evaledParentheses);
@@ -149,11 +184,11 @@ public class Web_Calculator {
             return num1 +  num2;
         else if(operator == '-')
             return num2 - num1;
-        else if(operator == '^')
-            return Math.pow(num2,num1);
+        else //(operator == '^')
+            return (float) Math.pow(num2,num1);
     }
 
-    //For getting operator priority (mult = div > sub = add)
+    //For getting operator priority (power > mult = div > sub = add)
     public static int priority(char c)
     {
         if(c == '^')
@@ -178,6 +213,7 @@ public class Web_Calculator {
         boolean lastCharDecimal = false;
         boolean numHasDecimal = false;
         boolean hasHangingParen = false;
+        boolean hasNumber = false;
         int numUnclosedParen = 0;
         //Single character expression
         if(expression.length() == 1)
@@ -191,6 +227,7 @@ public class Web_Calculator {
         for(int i = 0; i < expression.length(); i++)
         {
             char currChar = expression.charAt(i);
+            //System.out.println(i + " " + currChar);
             //Make sure no hanging decimals
             if(lastCharDecimal && !isNumber(currChar))
                 return 3;
@@ -219,6 +256,7 @@ public class Web_Calculator {
             //If it's a number, reset canBeOperator
             else if(isNumber(currChar))
             {
+                hasNumber = true;
                 //Zero handling
                 if(currChar == '0')
                 {
@@ -266,26 +304,35 @@ public class Web_Calculator {
             {
                 if(!expression.substring(i, i+4).equals("exp("))
                     return 2;
-                //No decimal followed by operator (e.g: 3.+5)
+                //No decimal followed by operator (e.g: 3.+exp(5))
                 if(lastCharDecimal)
                     return 3;
-                //Make sure no e+5 (invalid expression)
+                //Make sure no exp(+5) (invalid expression)
                 canBeOperator = false;
-                //Increment the counter to skip over the next two letters
+                //Make sure no 5exp(45) (invalid without operator)
+                if(i != 0 && isNumber(expression.charAt(i-1)))
+                    return 1;
+                //Increment the counter to skip over the next letter & reset lastCharDecimal
                 i+=2;
+                lastCharDecimal = false;
             }
             //Log
             else if(currChar == 'l')
             {
                 if(!expression.substring(i, i+3).equals("ln("))
                     return 2;
-                //No decimal followed by operator (e.g: 3.+5)
+                //No decimal followed by operator (e.g: 3.+ln(5))
                 if(lastCharDecimal)
                     return 3;
-                //Make sure no log+5 (invalid expression)
+                //Make sure no ln(+5) (invalid expression)
                 canBeOperator = false;
-                //Increment the counter to skip over the next two letters
+
+                //Make sure no 5ln(45) (invalid without operator)
+                if(i != 0 && isNumber(expression.charAt(i-1)))
+                    return 1;
+                //Increment the counter to skip over the next two letters & reset lastCharDecimal
                 i++;
+                lastCharDecimal = false;
             }
             //Parentheses
             else if(currChar == '(')
@@ -295,16 +342,22 @@ public class Web_Calculator {
             }
             else if(currChar == ')')
             {
+                lastCharDecimal = false;
                 if(!hasHangingParen)
                     return 5;
                 numUnclosedParen--;
                 if(numUnclosedParen == 0)
                     hasHangingParen = false;
+                if(isOperator(expression.charAt(i-1)))
+                    return 1;
             }
-                //If it isn't a valid character
+            //If it isn't a valid character
             else
                 return 2;
         }
+        //If no number in expression
+        if(!hasNumber)
+            return 1;
         //If uneven parenthesis
         if(numUnclosedParen != 0 || hasHangingParen)
             return 5;
